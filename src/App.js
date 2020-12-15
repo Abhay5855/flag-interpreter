d29b369d36c149b6d62838e626b355cb8033402f
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2020-12-15 15:50:29 +0530

    feat: add dictionary consisting of flag and ther names

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  var foodDB = {
+    "🍔": "ham-burger",
+    "🥮": "moon cake",
+    "🍨": "ice-cream",
+    "🥚": "egg",
+    "🌭": "hotdog",
+    "🥪": "sandwhich",
+    "🍗": "poultry leg"
+  };
   return (
     <div className="App">
-      <h1>Flag recognizer</h1>
-      <h2>click the flag to know which country</h2>
+      <h1>Food recognizer</h1>
+      <h2>click the food to know its name</h2>
     </div>
   );
 }
