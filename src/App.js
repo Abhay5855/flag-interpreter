351833564324c91528d2bab06fcd6195e96c32b5
Abhay Patil <71277980+Abhay5855@users.noreply.github.com>
2020-12-15 15:56:12 +0530

    feat : add input handler to handle the input

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ export default function App() {
   return (
     <div className="App">
       <h1>Food recognizer</h1>
+
+      <input onChange={flagInputHandler} />
       <h2>click the food to know its name</h2>
     </div>
   );
